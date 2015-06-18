28ad8b9cedfda7c8fb7b23f6c61c83f2a5068de6
Andrew Seward <awswrd@gmail.com>
2015-06-18 10:55:12 -0700

    Another bot

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var barelyKnowerBot = require('./barelyKnowerBot');
+var sophieBot = require('./sophieBot');
 
 var app = express();
 var port = process.env.PORT || 3000;
@@ -8,6 +9,8 @@ var port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/doesheknower', barelyKnowerBot);
+
+app.post('/sophie', sophieBot);
 
 app.use(function(err, req, res, next) {
   console.error(err.stack);
---
+++ sophieBot.js
@@ -0,0 +1,21 @@
+var wasSlackBot = function(username) {
+  return username === 'slackbot';
+}
+
+var containsChoice = function(str) {
+  var pattern = /^choice$|^choice[^\w]+|[^\w]+choice[^\w]+|[^\w]choice$/ig;
+
+  return pattern.test(str);
+}
+
+module.exports = function(req, res, next) {
+  var text = req.body.text;
+  var username = req.body.user_name;
+
+  if (wasSlackBot(username)) return res.status(200).end();
+  if (!containsChoice(text)) return res.status(200).end();
+
+  var payload = { text: "Sophie's choice!" };
+
+  return res.status(200).json(payload);
+};
