c616bffaa9c3d87dd62f67eb9e87785e8e7d72f2
Andrew Seward <awswrd@gmail.com>
2015-07-23 03:42:22 -0700

    Switch to babel, clean some things up
    The es6-transpiler-jest thing was preventing test failures from actually
    coming through as failures, so I decided to just move things to babel.
    Also added a failing test to sophieBot-test

--- __tests__/sophieBot-test.js
+++ __tests__/sophieBot-test.js
@@ -1,16 +1,19 @@
 "use strict";
 
 const srcPath = "../src/sophieBot.js";
-
 jest.dontMock(srcPath);
 
 describe("containsChoice", () => {
-  it("findes matches", () => {
-    const containsChoice = require(srcPath).containsChoice;
-    const expectedResult = true;
+  const containsChoice = require(srcPath).containsChoice;
 
-    expect(containsChoice("the choice is yours")).toEqual(expectedResult);
+  it("finds matches", () => {
+    expect(containsChoice("the choice is yours")).toBeTruthy();
   });
+
+  it("matches plural choices", () => {
+    expect(containsChoice("poor choices can ruin your life")).toBeTruthy();
+  });
+
 });
 
 // TODO: add more...
