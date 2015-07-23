78d9d7709627416f666382844b0f58f6a1034b2f
Andrew Seward <awswrd@gmail.com>
2015-07-23 01:21:48 -0700

    No longer need immediately-ex functions
    Must've been doing something wrong when eslint was screaming at me...

--- src/app.js
+++ src/app.js
@@ -1,24 +1,22 @@
-(function() {
-  "use strict";
+"use strict";
 
-  let express = require("express");
-  let bodyParser = require("body-parser");
-  let barelyKnowerBot = require("./barelyKnowerBot").bot;
-  let sophieBot = require("./sophieBot").bot;
+let express = require("express");
+let bodyParser = require("body-parser");
+let barelyKnowerBot = require("./barelyKnowerBot").bot;
+let sophieBot = require("./sophieBot").bot;
 
-  let app = express();
-  let port = process.env.PORT || 3000;
+let app = express();
+let port = process.env.PORT || 3000;
 
-  app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-  app.post("/doesheknower", barelyKnowerBot);
+app.post("/doesheknower", barelyKnowerBot);
 
-  app.post("/sophie", sophieBot);
+app.post("/sophie", sophieBot);
 
-  app.use((err, req, res) => {
-    console.error(err.stack);
-    res.status(400).send(err.message);
-  });
+app.use((err, req, res) => {
+  console.error(err.stack);
+  res.status(400).send(err.message);
+});
 
-  app.listen(port, () => console.log("Listening on port " + port));
-})();
+app.listen(port, () => console.log("Listening on port " + port));
--- src/barelyKnowerBot.js
+++ src/barelyKnowerBot.js
@@ -1,65 +1,63 @@
-(function() {
-  "use strict";
+"use strict";
 
-  var splitWord = require("./stringUtils").splitWord;
+let splitWord = require("./stringUtils").splitWord;
 
-  function getMatches(str) {
-    let pattern = /(\w{2,}er)[^\w]+|(\w{2,}er)$/gi;
+function getMatches(str) {
+  let pattern = /(\w{2,}er)[^\w]+|(\w{2,}er)$/gi;
 
-    return str.match(pattern) || [];
+  return str.match(pattern) || [];
+}
+
+function cleanMatches(matches) {
+  let pattern = /[^\w]+$/;
+
+  return matches.map(str => {
+    return str.replace(pattern, "");
+  });
+}
+
+function splitByEr(word) {
+  return splitWord(word, "er");
+}
+
+function formatSplitWordParts(parts) {
+  function capitalizeFirst(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
   }
 
-  function cleanMatches(matches) {
-    let pattern = /[^\w]+$/;
+  return capitalizeFirst(parts[0]) + " '" + parts[1] + "?!";
+}
 
-    return matches.map(str => {
-      return str.replace(pattern, "");
-    });
+function linkify(username) {
+  if (username.indexOf(".") !== -1) {
+    return "@" + username;
+  } else {
+    return "<@" + username + "|" + username + ">";
+  }
+}
+
+function bot(req, res) {
+  let text = req.body.text;
+  let username = req.body.user_name;
+  let matches = cleanMatches(getMatches(text));
+
+  if (username === "slackbot" || matches.length === 0) {
+    return res.status(200).end();
   }
 
-  function splitByEr(word) {
-    return splitWord(word, "er");
-  }
+  let responseMessage = matches
+    .map(splitByEr)
+    .map(formatSplitWordParts)
+    .join(" ");
 
-  function formatSplitWordParts(parts) {
-    function capitalizeFirst(str) {
-      return str.charAt(0).toUpperCase() + str.slice(1);
-    }
-
-    return capitalizeFirst(parts[0]) + " '" + parts[1] + "?!";
-  }
-
-  function linkify(username) {
-    if (username.indexOf(".") !== -1) {
-      return "@" + username;
-    } else {
-      return "<@" + username + "|" + username + ">";
-    }
-  }
-
-  function bot(req, res) {
-    let text = req.body.text;
-    let username = req.body.user_name;
-    let matches = cleanMatches(getMatches(text));
-
-    if (username === "slackbot" || matches.length === 0) {
-      return res.status(200).end();
-    }
-
-    let responseMessage = matches
-      .map(splitByEr)
-      .map(formatSplitWordParts)
-      .join(" ");
-
-    let payload = {
-      text: linkify(username) + ": " + responseMessage + " I barely know 'er!",
-    };
-
-    return res.status(200).json(payload);
-  }
-
-  module.exports = {
-    getMatches: getMatches,
-    bot: bot,
+  let payload = {
+    text: linkify(username) + ": " + responseMessage + " I barely know 'er!",
   };
-})();
+
+  return res.status(200).json(payload);
+}
+
+module.exports = {
+  getMatches: getMatches,
+  bot: bot,
+};
