c0202f778d63fbc971abb0523411c83dd3ff3b57
Andrew Seward <awswrd@gmail.com>
2015-07-22 22:08:51 -0700

    Don't really remember why this was in here
    I believe it was mostly just some cleanup

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 (function() {
   "use strict";
 
-  var express = require("express");
-  var bodyParser = require("body-parser");
-  var barelyKnowerBot = require("./barelyKnowerBot").bot;
-  var sophieBot = require("./sophieBot");
+  let express = require("express");
+  let bodyParser = require("body-parser");
+  let barelyKnowerBot = require("./barelyKnowerBot");
+  let sophieBot = require("./sophieBot");
 
-  var app = express();
-  var port = process.env.PORT || 3000;
+  let app = express();
+  let port = process.env.PORT || 3000;
 
   app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -15,12 +15,10 @@
 
   app.post("/sophie", sophieBot);
 
-  app.use(function(err, req, res) {
+  app.use((err, req, res) => {
     console.error(err.stack);
     res.status(400).send(err.message);
   });
 
-  app.listen(port, function() {
-    console.log("Slack bot listening on port " + port);
-  });
+  app.listen(port, () => console.log("Listening on port " + port));
 })();
