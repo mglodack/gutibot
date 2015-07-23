ffb7310d6b588ba76a629079ca27684bb80c680b
Brian Field <brian@madsalty.com>
2015-07-23 15:04:22 +1000

    Remove jsdocs

--- src/stringUtils.js
+++ src/stringUtils.js
@@ -1,10 +1,6 @@
 (function() {
   "use strict";
 
-  /**
-   * @param {string} word - The word to split
-   * @param {string} suffix - The suffix to split on
-   */
   function splitWord(word, suffix) {
     var indexOfSuffix = word.lastIndexOf(suffix);
     var prefix = word.slice(0, indexOfSuffix);
